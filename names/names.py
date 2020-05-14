438d4828a03abda63358e4e256d52c717dc37b5f
eyvonne <eyvonne@users.noreply.github.com>
2020-05-14 16:11:10 -0700

    completed sprint

--- names/names.py
+++ names/names.py
@@ -1,28 +1,54 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = f.read().split("\n")
 f.close()
+
+with open('names_1.txt', 'r') as f:
+    names = BinarySearchTree(f.readline())
+    for line in f.readlines():
+        names.insert(line.strip())
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_2:
+    if names.contains(name):
+        duplicates.append(name)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# the fastest thing I could find on the web
+start_time2 = time.time()
+duplicates = set(names_1) & set(names_2)  # I got this line from google
+
+end_time2 = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(list(duplicates))}\n\n")
+print(f"runtime: {end_time2 - start_time2} seconds")
+
+# stretch goal: use only lists
+
+start_time3 = time.time()
+duplicates = [i for i in names_1 if i in names_2]
+
+end_time3 = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(list(duplicates))}\n\n")
+print(f"runtime: {end_time3 - start_time3} seconds")
+
+
+# Original Runtime: O(n^2)
+# BST Runtime: O(n log n)? Possibly O(log n)?
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,16 +1,20 @@
 class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
-        self.next_node = next_node
+        self.next = next_node
+
+    def __str__(self):
+        return f'{self.value}'
 
     def get_value(self):
         return self.value
 
     def get_next(self):
-        return self.next_node
+        return self.next
 
     def set_next(self, new_next):
-        self.next_node = new_next
+        self.next = new_next
+
 
 class LinkedList:
     def __init__(self):
@@ -39,4 +43,29 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node:
+            if not node.next:
+                self.head = node
+                node.next = prev
+                print(node)
+            else:
+                self.reverse_list(node.next, node)
+                node.next = prev
+                print(node)
+
+    def print_list(self):
+        if self.head == None:
+            print(None)
+            return None
+        current = self.head
+        while current != None:
+            print(current)
+            current = current.next
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.add_to_head(3)
+    ll.add_to_head(2)
+    ll.add_to_head(1)
+    ll.reverse_list(ll.head, None)
